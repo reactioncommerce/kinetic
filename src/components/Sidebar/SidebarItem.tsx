473e3dc50653c487f48316ab919ea158cd175742
Chloe <pinkcloudvnn@gmail.com>
2022-06-30 16:16:44 +0700

    Shop setting menu
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -4,35 +4,61 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { NavLink, useMatch, useResolvedPath } from "react-router-dom";
 
-type SidebarItemProps = {
-  to: string
+export type SidebarItemProps = {
+  to?: string
   text: string
   icon: JSX.Element
+  onClick?: () => void
 }
 
-export const SidebarItem = ({ to, text, icon }: SidebarItemProps) => {
+type NavLinkSidebarItemProps = Omit<SidebarItemProps, "to" | "onClick"> & {
+  to: string
+}
+
+const NavLinkSidebarItem = ({ to, text, icon }: NavLinkSidebarItemProps) => {
   const resolvedPath = useResolvedPath(to);
   const match = useMatch({ path: resolvedPath.pathname, end: true });
-
   return (
-    <ListItem dense sx={{ padding: '4px 10px' }}>
-      <ListItemButton
-        selected={!!match}
-        component={NavLink}
-        to={to}
-        dense
-        sx={{
-          padding: '4px 10px',
-          borderRadius: '5px',
-          '&.active': {
-            bgcolor: 'background.darkGrey',
-            '&:hover': { bgcolor: 'background.darkGrey' },
-          },
-        }}
-      >
-        <ListItemIcon sx={{ color: 'white', minWidth: '0px', mr: '16px' }}>{icon}</ListItemIcon>
-        <ListItemText primary={text} />
-      </ListItemButton>
-    </ListItem>
+    <ListItemButton
+      selected={!!match}
+      component={NavLink}
+      to={to}
+      dense
+      sx={{
+        "padding": "4px 10px",
+        "borderRadius": "5px",
+        "&.active": {
+          "bgcolor": "background.darkGrey",
+          "&:hover": { bgcolor: "background.darkGrey" }
+        }
+      }}
+    >
+      <ListItemIcon sx={{ color: "white", minWidth: "0px", mr: "16px" }}>{icon}</ListItemIcon>
+      <ListItemText primary={text} />
+    </ListItemButton>
   );
 };
+
+export const SidebarItem = ({ to, onClick, ...props }: SidebarItemProps) => (
+  <ListItem dense sx={{ padding: "4px 10px" }}>
+    {to ? (
+      <NavLinkSidebarItem to={to} {...props} />
+    ) : (
+      <ListItemButton
+        dense
+        sx={{
+          "padding": "4px 10px",
+          "borderRadius": "5px",
+          "&.active": {
+            "bgcolor": "background.darkGrey",
+            "&:hover": { bgcolor: "background.darkGrey" }
+          }
+        }}
+        onClick={onClick}
+      >
+        <ListItemIcon sx={{ color: "white", minWidth: "0px", mr: "16px" }}>{props.icon}</ListItemIcon>
+        <ListItemText primary={props.text} />
+      </ListItemButton>
+    )}
+  </ListItem>
+);
