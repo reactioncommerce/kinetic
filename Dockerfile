dc7f9c1ee0a6c0d0ce4cd09b580140688eb195b8
Chloe <pinkcloudvnn@gmail.com>
2022-09-12 19:14:35 +0700

    Update Dockerfile to use for production mode
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14.18.1-slim as development
+FROM node:14.18.1-slim as build
 
 WORKDIR /tmp
 RUN npm i -g pnpm@7
@@ -10,4 +10,16 @@ RUN pnpm fetch
 ADD . ./
 RUN pnpm install
 
-CMD ["pnpm", "dev"]
+RUN pnpm build
+
+
+FROM nginx:1.21.0-alpine
+ENV NODE_ENV production
+
+COPY --from=build /app/dist /usr/share/nginx/html
+
+COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/components/AppLogo/index.tsx
+++ src/components/AppLogo/index.tsx
@@ -1,6 +1,9 @@
 import Box from "@mui/material/Box";
 import { SxProps } from "@mui/material/styles";
 
+import fullWhiteLogo from "../../full-white-logo.svg";
+import fullLogo from "../../full-logo.svg";
+
 type AppLogoProps = {
   theme: "light" | "dark";
   sx?: SxProps;
@@ -10,6 +13,6 @@ export const AppLogo = ({ theme, sx }: AppLogoProps) => (
   <Box
     sx={{ width: "fit-content", height: "2.5rem", ...sx }}
     component="img"
-    src={theme === "light" ? "/src/full-white-logo.svg" : "/src/full-logo.svg" }
+    src={theme === "light" ? fullWhiteLogo : fullLogo}
     alt="Kinetic Logo"/>
 );
