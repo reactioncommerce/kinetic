e6847f5acac981438b557cbad9cecc9950c574f6
Chloe <pinkcloudvnn@gmail.com>
2022-10-27 14:28:48 +0700

    implement breadcrumb items
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- src/components/Breadcrumb/defaultBreadcrumbItems.ts
+++ src/components/Breadcrumb/defaultBreadcrumbItems.ts
@@ -5,9 +5,17 @@ export const BREADCRUMB_ITEMS = {
   "/promotions": "Promotions",
   "/categories": "Categories",
   "/settings": "Settings",
-  "/settings/users-and-permissions": "Users & Permissions",
+  "/settings/users": "Users",
+  "/settings/users/invitations": "Invitations",
+  "/settings/users/groups": "Groups",
   "/settings/shop-details": "Shop Details",
+  "/settings/shop-details/localization": "Localization",
   "/settings/checkout": "Checkout",
+  "/settings/checkout/taxes": "Taxes",
+  "/settings/checkout/address-validation": "Address Validation",
   "/settings/shipping-fulfillment": "Shipping & Fulfillment",
-  "/settings/emails": "Transactional Emails"
+  "/settings/shipping-fulfillment/restrictions": "Restrictions",
+  "/settings/shipping-fulfillment/surcharges": "Surcharges",
+  "/settings/emails": "Transactional Emails",
+  "/settings/emails/logs": "Logs"
 };
