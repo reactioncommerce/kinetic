d175fad8a0817a752c706f37b408827496af07eb
Chloe <pinkcloudvnn@gmail.com>
2022-11-19 14:12:12 +0700

    set up actions field
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- src/pages/Promotions/constants.ts
+++ src/pages/Promotions/constants.ts
@@ -1,9 +1,10 @@
+import { Stackability } from "@graphql/generates";
 import { SelectOptionType } from "types/common";
 import { CalculationType, PromotionType } from "types/promotions";
 
-export const CALCULATION_OPTIONS: Record<CalculationType, SelectOptionType<CalculationType>> = {
-  percentage: { label: "% Off", value: "percentage" },
-  amount: { label: "$ Off", value: "amount" }
+export const CALCULATION_OPTIONS: Record<CalculationType, SelectOptionType<CalculationType> & {symbol: string}> = {
+  percentage: { label: "% Off", value: "percentage", symbol: "%" },
+  amount: { label: "$ Off", value: "amount", symbol: "$" }
 };
 
 export const PROMOTION_TYPE_OPTIONS: Record<PromotionType, SelectOptionType<PromotionType>> = {
@@ -13,3 +14,8 @@ export const PROMOTION_TYPE_OPTIONS: Record<PromotionType, SelectOptionType<Prom
 };
 
 export const DATE_FORMAT = "yyyy-MM-dd";
+
+export const PROMOTION_STACKABILITY_OPTIONS: SelectOptionType<Stackability>[] = [
+  { label: "Never Stackable", value: Stackability.None },
+  { label: "Stack All", value: Stackability.All }
+];
