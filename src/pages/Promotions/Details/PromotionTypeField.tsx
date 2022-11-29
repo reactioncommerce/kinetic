4c807c268c084a892141a232a63395b731c8788e
Chloe <pinkcloudvnn@gmail.com>
2022-11-29 16:04:05 +0700

    small fix on change promotion type
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- src/pages/Promotions/Details/PromotionTypeField.tsx
+++ src/pages/Promotions/Details/PromotionTypeField.tsx
@@ -2,13 +2,20 @@ import { Field, FieldProps, useFormikContext } from "formik";
 
 import { SelectField } from "@components/SelectField";
 import { DISCOUNT_TYPES_MAP, PROMOTION_TYPE_OPTIONS } from "../constants";
-import { Promotion, PromotionType } from "types/promotions";
+import { CalculationType, Promotion, PromotionType } from "types/promotions";
 
 export const PromotionTypeField = () => {
   const { setFieldValue, values } = useFormikContext<Promotion>();
   const onPromotionTypeChange = (value: string) => {
     setFieldValue("promotionType", value);
-    values.actions.map((_, index) => setFieldValue(`actions[${index}].actionParameters.discountType`, DISCOUNT_TYPES_MAP[value as PromotionType]));
+    values.actions.forEach((action, index) => {
+      setFieldValue(`actions[${index}].actionParameters.discountType`, DISCOUNT_TYPES_MAP[value as PromotionType]);
+      setFieldValue(`actions[${index}].actionParameters.inclusionRules`, {});
+      setFieldValue(`actions[${index}].actionParameters.exclusionRules`, {});
+      if ((value === PromotionType.ItemDiscount || value === PromotionType.OrderDiscount) && action.actionParameters?.discountCalculationType === "flat") {
+        setFieldValue(`actions[${index}].actionParameters.discountCalculationType`, CalculationType.Percentage);
+      }
+    });
   };
 
   return (
