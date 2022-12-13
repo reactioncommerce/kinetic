7ddcdf66b52cbe270366326a15071da9d29d595b
Chloe <pinkcloudvnn@gmail.com>
2022-12-13 14:19:22 +0700

    fix error message
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- src/components/ErrorBoundary/GeneralError.tsx
+++ src/components/ErrorBoundary/GeneralError.tsx
@@ -8,7 +8,7 @@ type GeneralErrorProps = {
   description?: string
 }
 
-export const GeneralError = ({ title = "Something went wrong.", description = "Email you shop owner to get access." }:GeneralErrorProps) => (
+export const GeneralError = ({ title = "Something went wrong.", description = "Contact your shop owner to get access." }:GeneralErrorProps) => (
   <FullHeightLayout maxWidth="md">
     <AppLogo theme="dark" sx={{ mb: 3 }} />
     <Typography component="h1" variant="h4" gutterBottom>
