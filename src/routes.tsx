bff878cd8cf1a74a887b4171d0118e7783166501
Chloe <pinkcloudvnn@gmail.com>
2022-12-13 13:45:05 +0700

    remove accidental merge conflict code
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- src/routes.tsx
+++ src/routes.tsx
@@ -236,17 +236,6 @@ export const routes: RouteObject[] = [
                   }
                 ]
               },
-
-              {
-                path: "settings/shipping-fulfillment",
-                element: <PageLayout headers={shippingPageRoutes}/>,
-                children: shippingPageRoutes
-              },
-              {
-                path: "settings/emails",
-                element: <PageLayout headers={emailsSettingPageRoutes}/>,
-                children: emailsSettingPageRoutes
-              },
               {
                 path: "customers",
                 element: <PageLayout/>,
