46dd3ded48aa01bc7ae96cc897f1d953c50b5a97
Chloe <pinkcloudvnn@gmail.com>
2022-12-14 10:50:53 +0700

    fix end date validation
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- src/pages/Promotions/Details/AvailableDateField.tsx
+++ src/pages/Promotions/Details/AvailableDateField.tsx
@@ -1,6 +1,6 @@
 import Stack from "@mui/material/Stack";
-import { FastField, useFormikContext } from "formik";
-import { format, isBefore, isValid } from "date-fns";
+import { Field, useFormikContext } from "formik";
+import { format, isBefore } from "date-fns";
 
 import { DatePickerField } from "@components/DatePickerField";
 import { DATE_FORMAT } from "../constants";
@@ -9,23 +9,23 @@ import { Promotion } from "types/promotions";
 export const AvailableDateField = () => {
   const { setFieldValue, values } = useFormikContext<Promotion>();
 
-  const onStartDateChange = (value: Date | null) => {
+  const onStartDateAccept = (value: Date | null) => {
     setFieldValue("startDate", value ? format(value, DATE_FORMAT) : null);
-    if (value && isValid(value) && isBefore(new Date(values.endDate), new Date(value))) {
+    if (value && isBefore(new Date(values.endDate), new Date(value))) {
       setFieldValue("endDate", null);
     }
   };
 
   return (
     <Stack direction="row" gap={2} mt={1} position="relative" sx={{ width: { md: "50%", xs: "100%" } }} >
-      <FastField
+      <Field
         name="startDate"
         component={DatePickerField}
         label="Available From"
         inputFormat={DATE_FORMAT}
-        onAccept={onStartDateChange}
+        onAccept={onStartDateAccept}
       />
-      <FastField
+      <Field
         name="endDate"
         component={DatePickerField}
         label="Available To"
