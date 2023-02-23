dc783c99ee463aa182ce5ca51a5334043ec022f3
Chloe <pinkcloudvnn@gmail.com>
2023-02-23 09:17:03 +0700

    remove invalid discount limit values
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- src/pages/Promotions/Details/PromotionUsageLimits.tsx
+++ src/pages/Promotions/Details/PromotionUsageLimits.tsx
@@ -8,7 +8,7 @@ type PromotionUsageLimitsProps = {
 }
 
 export const PromotionUsageLimits = ({ actionParametersFieldName }: PromotionUsageLimitsProps) => (
-  <Stack sx={{ width: { lg: "50%", md: "100%" }, flexDirection: { lg: "row", md: "column" }, gap: { lg: 2, md: 0 } }} alignItems="center">
+  <Stack sx={{ width: { lg: "50%", md: "100%" }, flexDirection: { lg: "row", md: "column" }, gap: { lg: 2, md: 0 }, alignItems: { lg: "flex-start" } }}>
     <Field name={`${actionParametersFieldName}.discountMaxUnits`} component={TextField} type="number" label="Max # uses per Order" />
     <Field name={`${actionParametersFieldName}.discountMaxValue`} component={TextField} type="number" label="Max $ discount on an Order" />
   </Stack>
